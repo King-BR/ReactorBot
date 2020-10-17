f2c9015b6f47b44f848e2118f0cd401f8092c827
Fernando <fernandopach2017@gmail.com>
2020-10-16 21:55:12 -0300

    Update command template

--- templates/commandTemplate.js
+++ templates/commandTemplate.js
@@ -4,6 +4,7 @@ module.exports = {
     // Execução do comando
     run: (client, botUtils, message, args) => {
         newError = botUtils.newError;
+
         try {
             // Codigo do comando
         
@@ -11,8 +12,14 @@ module.exports = {
             let embed = new Discord.MessageEmbed()
                 .setTitle("Erro inesperado")
                 .setDescription("Um erro inesperado aconteceu. por favor contate os ADMs\n\nUm log foi criado com mais informações do erro");
-            message.channel.send(embed)
-            console.log(`=> ${newError(err, "nome do comando", message.guild.id)}`);
+            message.channel.send(embed);
+            
+            let IDs = {
+                server: message.guild.id,
+                user: message.author.id,
+                msg: message.id
+            }
+            console.log(`=> ${newError(err, "nome do comando", IDs)}`);
         }
     },
 
