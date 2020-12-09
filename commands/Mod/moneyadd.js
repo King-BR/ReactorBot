d50fb639882647442b5caa533b735b7783f16e7e
Rodrigo <60452545+rodrigolitzius@users.noreply.github.com>
2020-12-09 01:08:02 -0300

    Update moneyadd.js
    Terminei uma pasta yey

--- commands/Mod/moneyadd.js
+++ commands/Mod/moneyadd.js
@@ -3,13 +3,13 @@ const { Users } = require("../../database.js");
 
 module.exports = {
   run: async (client, botUtils, message, args) => {
-    if (!message.member.roles.cache.has('755604380295757824')) return message.reply("Você n é membro da STAFF");
+    if (!message.member.roles.cache.has('755604380295757824')) return message.reply("Você não é um membro STAFF.");
 
     newError = botUtils.newError;
 
     try {
 
-      if (isNaN(parseInt(args[1] || args[0]))) return message.reply('Não foi possivel indentificar a quantia de dinheiro a se informar');
+      if (isNaN(parseInt(args[1] || args[0]))) return message.reply('Não foi possível identificar a quantia de dinheiro a se informar.');
 
       let money = parseInt(args[1] || args[0])
       let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
@@ -25,7 +25,7 @@ module.exports = {
         try {
           let embedConfirm = new Discord.MessageEmbed()
             .setTitle(`Tem certeza que adicionar ${money}$ ao ${user.displayName || "`desconhecido`"}?`)
-            .setDescription("Reaja com ✅ para confirmar\nReaja com ❌ para cancelar")
+            .setDescription("Reaja com ✅ para confirmar.\nReaja com ❌ para cancelar.")
           message.channel.send(embedConfirm).then(msg => {
             msg.react("✅").then(() => {
               msg.react("❌");
@@ -38,10 +38,10 @@ module.exports = {
                 switch (r.emoji.name) {
                   case "✅": {
                     let embedReset = new Discord.MessageEmbed()
-                      .setDescription(`Dinheiro de ${user.displayName || "`desconhecido`"} foi adicionado de ${doc.money} para ${doc.money + money}`);
+                      .setDescription(`O dinheiro de ${user.displayName || "`desconhecido`"} foi adicionado de ${doc.money} para ${doc.money + money}`);
                       
                     let embedLog = new Discord.MessageEmbed()
-                      .setTitle("Dinheiro adicionado")
+                      .setTitle("Dinheiro adicionado!")
                       .setAuthor(message.author.tag,message.author.displayAvatarURL())
                       .setThumbnail(user.user.displayAvatarURL({format:'png',dynamic:true}))
                       .setDescription(`${user.user.tag || "`desconhecido`"} ganhou ${money}$, (${doc.money}$ -> ${doc.money + money}$)`);
@@ -56,7 +56,7 @@ module.exports = {
                   }
                   case "❌": {
                     let embedCancelado = new Discord.MessageEmbed()
-                      .setDescription(`Set do dinheiro de ${user.displayName || "`desconhecido`"} cancelado`);
+                      .setDescription(`Adição do dinheiro de ${user.displayName || "`desconhecido`"}foi cancelada.`);
                     msg.edit(embedCancelado);
                     reactionCollector.stop();
                     break;
@@ -68,7 +68,7 @@ module.exports = {
         } catch (err2) {
           let embed = new Discord.MessageEmbed()
             .setTitle("Erro inesperado")
-            .setDescription("Um erro inesperado aconteceu. por favor contate os ADMs\n\nUm log foi criado com mais informações do erro");
+            .setDescription("Um erro inesperado aconteceu. por favor contate os Desenvolvedores do ReactorBot.\n\nUm log foi criado com mais informações do erro.");
           message.channel.send(embed);
 
           let IDs = {
@@ -83,7 +83,7 @@ module.exports = {
     } catch (err) {
       let embed = new Discord.MessageEmbed()
         .setTitle("Erro inesperado")
-        .setDescription("Um erro inesperado aconteceu. por favor contate os ADMs\n\nUm log foi criado com mais informações do erro");
+        .setDescription("Um erro inesperado aconteceu. por favor contate os Desenvolvedores do ReactorBot.\n\nUm log foi criado com mais informações do erro.");
       message.channel.send(embed);
 
       let IDs = {
@@ -97,9 +97,9 @@ module.exports = {
 
   config: {
     name: "moneyadd",
-    noalias: "Sem sinonimos",
+    noalias: "Sem sinônimos",
     aliases: [],
-    description: "adiciona adinheiro na conta de um jogador",
+    description: "Adiciona dinheiro na conta de um membro do servidor.",
     usage: "moneyadd [Membro] <Quantia>",
     accessableby: "STAFF"
   }
