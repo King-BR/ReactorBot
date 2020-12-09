6fff2258f9b7cade9a638b57e38b8c2b9ee90156
Rodrigo <60452545+rodrigolitzius@users.noreply.github.com>
2020-12-08 21:49:01 -0300

    Testando

--- commands/Mod/kick.js
+++ commands/Mod/kick.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports = {
   // Execução do comando
   run: async (client, botUtils, message, args) => {
-    if (!message.member.roles.cache.has('755604380295757824')) return message.reply("Você n é membro da STAFF");
+    if (!message.member.roles.cache.has('755604380295757824')) return message.reply("Você não tem permissão para executar este comando.");
 
     newError = botUtils.newError;
     try {
@@ -37,7 +37,7 @@ module.exports = {
     } catch(err) {
       let embed = new Discord.MessageEmbed()
         .setTitle("Erro inesperado")
-        .setDescription("Um erro inesperado aconteceu. por favor contate os ADMs\n\nUm log foi criado com mais informações do erro");
+        .setDescription("Um erro inesperado aconteceu. por favor contate os Desenvolvedores do ReactorBot.\n\nUm log foi criado com mais informações do erro.");
       message.channel.send(embed);
 
       let IDs = {
@@ -51,7 +51,7 @@ module.exports = {
   // Configuração do comando
   config: {
     name: "kick",
-    noalias: "Sem sinonimos",
+    noalias: "Sem sinônimos",
     aliases: [],
     description: "De um kick em um membro do server",
     usage: "kick <@member> [motivo]",
