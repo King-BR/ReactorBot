756e407aa1f5e3a9691768a5b4285d9a7f4dd6fa
Rodrigo <60452545+rodrigolitzius@users.noreply.github.com>
2020-12-08 22:59:47 -0300

    Update purge.js
    "apage"... Essa foi de fuder hein Alstin...

--- commands/Mod/purge.js
+++ commands/Mod/purge.js
@@ -3,14 +3,14 @@ const Discord = require("discord.js");
 module.exports = {
   // Execução do comando
   run: (client, botUtils, message, args) => {
-    if (!message.member.roles.cache.has('755604380295757824')) return message.reply("Você n é membro da STAFF");
+    if (!message.member.roles.cache.has('755604380295757824')) return message.reply("Você não é um membro STAFF.");
     
     newError = botUtils.newError;
     try {
       // Codigo do comando
-      if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("Você não tem permissão para isso");
-      if(typeof parseInt(args[0]) != "number") return message.channel.send("Quantidade invalida");
-      if(parseInt(args[0]) < 1) return message.channel.send("O numero precisa ser maior que 0");
+      if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("Você não tem permissão para executar este comando.");
+      if(typeof parseInt(args[0]) != "number") return message.channel.send("Número inválido.");
+      if(parseInt(args[0]) < 1) return message.channel.send("O número precisa ser maior que 0.");
 
 		  message.channel.bulkDelete(parseInt(args[0]) + 1)
         .then(messages => message.channel.send(`Foram deletadas ${messages.size - 1} mensagens.`))
@@ -19,7 +19,7 @@ module.exports = {
     } catch(err) {
       let embed = new Discord.MessageEmbed()
         .setTitle("Erro inesperado")
-         .setDescription("Um erro inesperado aconteceu. por favor contate os ADMs\n\nUm log foi criado com mais informações do erro");
+         .setDescription("Um erro inesperado aconteceu. por favor contate os Desenvolvedores do ReactorBot.\n\nUm log foi criado com mais informações do erro.");
       message.channel.send(embed);
 
       let IDs = {
@@ -33,9 +33,9 @@ module.exports = {
   // Configuração do comando
   config: {
     name: "purge",
-    noalias: "Sem sinonimos",
+    noalias: "Sem sinônimos",
     aliases: [],
-    description: "Apage mensagens do canal",
+    description: "Use para apagar mensagens do servidor.",
     usage: "purge <quantidade>",
     accessableby: "STAFF"
   }
